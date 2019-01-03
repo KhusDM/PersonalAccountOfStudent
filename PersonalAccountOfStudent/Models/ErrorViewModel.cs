706aa425e8e8d736431bf1d505d33f3604e2a6b5
KhusnetdinovDmitrii <d.xusnetd@yandex.ru>
2019-01-03 22:18:42 +0300

    Add project files.

---
+++ PersonalAccountOfStudent/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PersonalAccountOfStudent.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
